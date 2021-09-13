922ffd9db15f266fd3ab8e9be1a57336d5dbb2dd
Ryan Szczepanski <rszczepanski@comcast.net>
2021-09-13 05:08:29 -0400

    output team

--- index.js
+++ index.js
@@ -203,8 +203,8 @@ function main() {
     }
     function outputTeam() {
         // Create the output directory if the output path doesn't exist
-        if (!fs.existsSync(OUTPUT_DIR)) {
-            fs.mkdirSync(OUTPUT_DIR)
+        if (!fs.existsSync(output_dir)) {
+            fs.mkdirSync(output_dir)
         }
         fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
     }
