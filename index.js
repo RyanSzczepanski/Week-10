7ce985f7db625a67b26312f0d293135a5f0348b6
Ryan Szczepanski <rszczepanski@comcast.net>
2021-09-13 03:55:28 -0400

    Main Page HTML

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// let employee = new Intern("Name", 0, "employee@email.com", "Github")
+// console.log(`Name: ${employee.getName()}
+// Id: ${employee.getId()}
+// Email: ${employee.getEmail()}
+// Role: ${employee.getRole()}
+// School: ${employee.getSchool()}`);
+
+
