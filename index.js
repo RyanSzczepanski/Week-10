82b016449a37eb61541d9ee364a1de9c30ed3802
Ryan Szczepanski <rszczepanski@comcast.net>
2021-09-13 04:51:40 -0400

    Manager Inquirer

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const inquirer = require('inquirer');
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const output_dir = path.resolve(__dirname, "output")
+const outputPath = path.join(output_dir, "team.html");
 const teamMembers = [];
 const idArray = [];
 
